70400d9f18c1caa749080c028fe783382734a437
avanegasp <arco527@gmail.com>
2017-06-29 22:36:54 -0500

    Puzzle is working

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
-$(function(){
-  
-})
+$(document).on('ready',iniciar);
+
+  var enlace;
+
+  function iniciar(){
+    $('#tabla2 div img').on('click', copiar);
+  }
+
+  function copiar(){
+    enlace=$(this).attr('src');
+    $('#tabla1 div img').on('click', pegar)
+  }
+
+  function pegar (){
+    $(this).attr('src', enlace);
+  }
