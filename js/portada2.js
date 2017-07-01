3861f457748c94898810f3302a42da7fc6af3af5
avanegasp <arco527@gmail.com>
2017-06-30 20:21:09 -0500

    Add draggable event

---
+++ js/portada2.js
@@ -0,0 +1,24 @@
+// $(document).on('ready',iniciar);
+//
+//   var enlace;
+//
+//   function iniciar(){
+//     $('#tabla2 div img').draggable(copiar);
+//   }
+//
+//   function copiar(){
+//     enlace=$(this).attr('src');
+//     $('#tabla1 div img').on('draggable', pegar)
+//   }
+//
+//   function pegar (){
+//     $(this).attr('src', enlace);
+//   }
+
+$( function() {
+   $( "#tabla2 div img" ).draggable();
+ });
+
+$(function(){
+  $('h1').draggable();
+})
