a2680cda4af518ecd376425b7184a607d22e2dee
avanegasp <arco527@gmail.com>
2017-07-02 23:19:37 -0500

    Put two bottons more

--- js/app1.js
+++ js/app1.js
@@ -1,20 +1,26 @@
 // $(document).on('ready',iniciar);
+  //
+  // $(function() {
+  //   var imagenes = ['img1.png', 'img2.png', 'img3.png', 'img4.png', 'img 5.png', 'img6.png'];
+  //   $('#tabla1').css({'img': 'url(img6/' + imagenes[Math.floor(Math.random() * imagenes.length)] + ')'});
+  // });
+//   var enlace;
+//
+//   function iniciar(){
+//     $('#tabla2 div img').on('click', copiar);
+//   }
+//
+//   function copiar(){
+//     enlace=$(this).attr('src');
+//     $('#tabla1 div img').on('click', pegar)
+//   }
+//
+//   function pegar (){
+//     $(this).attr('src', enlace);
+// }
 
-  $(function() {
-    var imagenes = ['img1.png', 'img2.png', 'img3.png', 'img4.png', 'img 5.png', 'img6.png'];
-    $('#tabla1').css({'background-image': 'url(img6/' + imagenes[Math.floor(Math.random() * imagenes.length)] + ')'});
+
+
+$("#right").click(function(){
+        $("#tabla2").animate({backgroundPositionX:"500px"},5000);
   });
-  // var enlace;
-  //
-  // function iniciar(){
-  //   $('#tabla2 div img').on('click', copiar);
-  // }
-  //
-  // function copiar(){
-  //   enlace=$(this).attr('src');
-  //   $('#tabla1 div img').on('click', pegar)
-  // }
-  //
-  // function pegar (){
-  //   $(this).attr('src', enlace);
-  // }
