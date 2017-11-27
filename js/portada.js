556977616b9bab1f7d3b4516e1ab7ba622d92205
avanegasp <arco527@gmail.com>
2017-11-27 09:41:01 -0500

    Puzzle Mario Bros

--- js/portada.js
+++ js/portada.js
@@ -1,12 +1,19 @@
-$(function(){
-  $('a').draggable(),
-  $('#hongo').draggable()
-})
+// $(function(){
+//   $('a').draggable()
+// })
+//
+//
+// $(function(){
+//   $('a').click(function(){
+//     $("a").load('index.html'),
+//     $("a").load('index2.html')
+//   })
+// })
 
 
-$(function(){
-  $('a').click(function(){
-    $("a").load('index.html'),
-    $("a").load('index2.html')
-  })
-})
+var slider = $('#slider');
+var siguiente =$('#btn-next');
+var anterior =$('#btn-prev');
+
+//mover ultima imagen al primer lugar
+$('#slider section:last').insertBefore('#slider section:first')
